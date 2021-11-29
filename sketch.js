4bca72ae22a917a95e0b3cc3d7a8a17ee0385451
Qcsmith03 <91340462+Qcsmith03@users.noreply.github.com>
2021-11-29 12:11:09 -0600

    attempt to add horse screen

--- sketch.js
+++ sketch.js
@@ -12,6 +12,18 @@ function setup() {
 function draw(){
   background(0);
   if (state ==="whatToBetScreen"){
-    background(50);
+    background(255);
+    fill("black");
+    textSize(50);
+    text("What to bet on!",500,100);
+    text("Horses",500,200);
+    text("Boxers",500,300);
+    text("Rats",500,400);
+    text("funds;500",100,500);
+    
+  }
+  if (state === "horseBetting"){
+    background(0);
   }
 }
+
