af0dc634f9ea261727842436af1d5fa6b7d1f5e7
Qcsmith03 <91340462+Qcsmith03@users.noreply.github.com>
2021-11-30 12:10:14 -0600

    changes, make the horse bet work

--- sketch.js
+++ sketch.js
@@ -2,7 +2,16 @@
 //quinten smith
 
 let state;
+let horse1;
+let horse2;
+let horse3;
 let music;
+
+function preload(){
+  horse1 = loadImage("assets/horse.png");
+  horse2 = loadImage("assets/horse2.png");
+  horse3 = loadImage("assets/horse3.png");
+}
 
 function setup() {
   createCanvas(windowWidth,windowHeight);
@@ -15,7 +24,7 @@ function draw(){
     background(255);
     fill("black");
     textSize(50);
-    text("What to bet on!",500,100);
+    text("What to bet on:",500,100);
     text("Horses",500,200);
     text("Boxers",500,300);
     text("Rats",500,400);
@@ -23,7 +32,17 @@ function draw(){
     
   }
   if (state === "horseBetting"){
-    background(0);
+    background(255);
+    image(horse1,1300,500,200,200);
+    image(horse2,1300,300,200,200);
+    image(horse3,1300,100,200,200);
   }
 }
 
+function mousePressed(){
+  if (mouseX === 500 && mouseY === 200){
+    state = "horseBetting";  
+  }
+  
+
+}
